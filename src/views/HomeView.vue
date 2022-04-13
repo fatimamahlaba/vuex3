d7934f676ddc032e5bcdd9b521b03ef7dcc2c0d9
Fatima <fatimamahlaba@gmail.com>
2022-04-13 14:57:35 +0200

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  <counter />
+  <counter-squared />
+  <buttons />
+  <color-code />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'HomeView',
+ components: {
+   'counter': require(`@/components/Counter.vue`).default,
+   'counter-squared': require(`@/components/CounterSquared.vue`).default,
+   'buttons': require(`@/components/Buttons.vue`).default,
+   'color-code': require(`@/components/ColorCode.vue`).default,
+ },
+
+}
+</script>
+<style scoped>
+div{
+  margin-bottom: 10px;
+}
+.counter{
+  font-size: 80px;
+}
+.button button {
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+}
+</style>
